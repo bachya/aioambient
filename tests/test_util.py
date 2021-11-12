19042b2e347d89049e6bbdf384c0ea5839fda1f9
Aaron Bach <bachya1208@gmail.com>
2021-11-12 16:11:58 -0700

    Add utility to get a public device ID

---
+++ tests/test_util.py
@@ -0,0 +1,11 @@
+"""Define tests for utilities."""
+import pytest
+
+from aioambient.util import get_public_device_id
+
+
+@pytest.mark.asyncio
+async def test_get_public_id():
+    """Test getting the public ID of a device by its MAC address."""
+    public_id = get_public_device_id("AB:CD:EF:12:34:56")
+    assert public_id == "04629a94fef5bfb62b525a6784cb8b37"
